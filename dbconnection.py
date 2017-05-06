9dd1c35cdc289d311045b40dddfe3563cfc7ce7b
MarkTigchelaar <tigchelaarm@mymacewan.ca>
2017-05-06 11:30:37 -0600

    remove duplicates

--- dbconnection.py
+++
@@ -1,8 +0,0 @@
-import MySQLdb
-
-#You must specify your localhost, login name, password for MySQL
-#as well as the Database you are inserting into.
-#This is to be setup beforehand while logged into MySQL.
-def dbstuff():
-    database = MySQLdb.connect('localhost', 'usrname', 'password', 'Database name')
-    return database.cursor(), database
