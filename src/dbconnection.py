2b7b1d7d412a582f0f51e7925cd941b46e2faa99
MarkTigchelaar <tigchelaarm@mymacewan.ca>
2017-05-06 11:33:04 -0600

    update

--- src/dbconnection.py
+++ src/dbconnection.py
@@ -1,8 +1,8 @@
 import MySQLdb
 
-#You must specify your localhost, login name, and shorthand name for MySQL
+#You must specify your localhost, login name, password for MySQL
 #as well as the Database you are inserting into.
 #This is to be setup beforehand while logged into MySQL.
 def dbstuff():
-    database = MySQLdb.connect('localhost', 'Mark', 'MKPC', 'DataScience')
+    database = MySQLdb.connect('localhost', 'usrname', 'password', 'Database name')
     return database.cursor(), database
